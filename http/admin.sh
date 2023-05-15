0480510b2b6fbc0ce03c6143ab2ce6427a5eefc6
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-05-15 11:02:57 +0300

    - Add grep for better parameter validation. - Add User ID query code (SQLi vulnerable). - Add experimental picture upload code.

--- http/admin.sh
+++ http/admin.sh
@@ -2,17 +2,20 @@
 function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 
 AUTHENTICATED="no"
-AUTH_TOKEN=`echo $HTTP_COOKIE | cut -f 2 -d =`
+AUTH_TOKEN=`echo $HTTP_COOKIE | grep -w 'auth_token' | cut -f 2 -d =`
+
 # Check if the user is already authenticated
+# Hardcoding tokens is a terrible idea, which is the goal of a vulnerable VM, right?
 if [ $AUTH_TOKEN = '5d6071b8e93644c987409f07937ed873' ]; then
     AUTHENTICATED="yes"
 else
-    if [ "$REQUEST_METHOD" = "POST" ]; then
-        # Read the query from stdin and decode it
-        QUERY=`cat`
+	if [ "$REQUEST_METHOD" = "POST" ]; then
+    	# Read the query from stdin and decode it
+    	QUERY=`cat`
         QUERY=`urldecode "$QUERY"`
         
         # format: username=...&password=...
+        # The first cut isolates the query parameter, grep makes sure we are getting the expected parameter, the last cut gets the value
         USERNAME=`echo $QUERY | cut -f 1 -d \& | grep -w 'username' | cut -f 2 -d =`
         PASS=`echo $QUERY | cut -f 2 -d \& | grep -w 'password' | cut -f 2 -d =`
 
@@ -30,8 +33,30 @@ echo '<head>'
 echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
 echo '<title>Administration page</title>'
 echo '</head>'
-echo '<body>'
 if [ $AUTHENTICATED = "yes" ]; then
-    echo '<h1>Admin dashboard</h1><br>
-    <h2>Profile photo</h2><img src="../uploads/profile.png"><br>
+	echo '<body>
+  <h1>Admin dashboard</h1><br>
+  The page is still a work in progress.<br>
+  <h2>Profile photo</h2>
+  <img src="../uploads/profile.png"><br>
+  Change image
+  <form method="post" enctype="multipart/form-data" action="img_upload.cgi">
+    <label for="file">File</label>
+    <input id="file" name="file" type="file"><br>
+    <button>Upload</button><br>
+  </form>
 
+  <h2>Query user ID</h2>
+  <form action=login.cgi>
+    <label for="id">User ID</label><br>
+    <input type="text" name="id"><br>
+    <input type="submit" value="Go!">
+  </form>'
+	if [ $REQUEST_METHOD = "GET" ]; then
+		QUERY_STRING=`urldecode $QUERY_STRING`
+		ID=`echo "$QUERY_STRING" | grep -w 'id' | cut -f 2 -d =`
+		if [ -n "$ID" ]; then
+			RESULT=`/usr/bin/mysql -h localhost -u web_user -e "USE web_service; SELECT * FROM creds WHERE ID = $ID;"`
+			echo "$RESULT"
+	fi
+fi
