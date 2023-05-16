fd9cffc44e3d187409d3161bb7bb21b36eec7243
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-05-16 21:02:42 +0300

    Works now

--- http/ping.sh
+++ http/ping.sh
@@ -11,7 +11,7 @@ echo '<title>Latency test</title>'
 echo '</head>'
 echo '<body>
 <h1>Enter the number of pings used for latency testing:</h1>
-<form action="ping.cgi">
+<form action="ping.sh">
 <label for="count">Number of pings</label>
 <input id="count" name="count" type="text">
 <input type="submit" value="Run!">
@@ -22,9 +22,11 @@ if [ -n "$QUERY_STRING" ]; then
     $QUERY_STRING=`urldecode "$QUERY_STRING"`
     COUNT=`echo "$QUERY_STRING" | cut -f 2 -d =`
     # Check if the count is actually only numbers
-    if [[ $1 =~ ^[0-9]+$ ]]; then
-        ping $SOURCE_IP -c $COUNT
+    if [[ "$COUNT" =~ ^[0-9]+$ ]]; then
+        RESULT=`ping $REMOTE_ADDR -c $COUNT`
+        echo -e "<div style='white-space:pre-wrap;'><tt>\n$RESULT\n</tt></div>"
     else
         echo "Invalid count parameter"
     fi
 fi
+echo '</body></html>'
