b3464b43847ad4c7798294a38703c8b35c0c9a6c
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-05-18 19:19:26 +0300

    Remove infinite loop

--- systemd/restricted.sh
+++ systemd/restricted.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-while true
-do
-  su - restricted -c 'ncat -k -c "firejail --profile=/etc/firejail/only-home.profile" -l -p 40000'
-  sleep 1
-done
+su - restricted -c 'ncat -k -c "firejail --profile=/etc/firejail/only-home.profile" -l -p 40000'
