efb113ac2097fc2b6729bddf7e94895b5737e994
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-07-10 12:28:35 +0300

    The main setup script.

---
+++ setup.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+if [ $EUID -ne 0 ]; then
+    echo "You must run this as root, try with sudo."
+    exit 1
+fi
+
+function get_section {
+    awk -v "section=[$1]" -f get_configuration.awk auto-setup.conf
+}
+
+function exit_on_fail {
+    if [ $1 -ne 0 ]; then
+        exit 1
+    fi
+}
+
+system_type=$(get_section System)
+add_packages=$(get_section 'Add Packages')
+remove_packages=$(get_section 'Remove Packages')
+req_flatpacks=$(get_section Flatpak)
+system_units=$(get_section 'System Units')
+all_users_units=$(get_section 'User Units')
+files_mapping=$(get_section Files)
+pre_script=$(get_section Pre)
+post_script=$(get_section Post)
+post_package_install=$(get_section 'Post Packages')
+
+if [ -n "$add_packages" ] && [ -n "$req_flatpacks" ]; then
+    echo "Checking network connectivity..."
+    ping -c 4 8.8.8.8 &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Connect to the internet and try again."
+        exit 2
+    fi
+fi
+
+# Execute the pre-install script
+if [ -n "$pre_script" ]; then
+    ./"$pre_script" "$system_type"
+fi
+
+# Install/Remove packages depending on your system type
+if [ -n "$system_type" ]; then
+    # Fedora and derivatives
+    if [ "$system_type" == "rpm" ]; then
+        dnf install $add_packages -y
+
+        if [ -n "$req_flatpacks" ]; then
+            dnf install flatpak
+        fi
+
+        dnf remove $remove_packages -y &&\
+        dnf upgrade -y
+        exit_on_fail $?
+    fi
+    # Debian/Ubuntu derivatives
+    if [ "$system_type" == "deb" ]; then
+        apt-get update &&\
+        apt-get install $add_packages -y
+        exit_on_fail $?
+
+        if [ -n "$req_flatpacks" ]; then
+            apt-get install flatpak -y
+        fi
+
+        apt-get remove --purge $remove_packages -y &&\
+        apt-get autoremove -y &&\
+        apt-get upgrade -y
+        exit_on_fail $?
+    fi
+fi
+
+# Install Flatpaks
+if [ -n "$req_flatpacks" ]; then
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak install $req_flatpacks -y
+fi
+
+# Execute post package install script
+if [ -n "$post_package_install" ]; then
+    ./"$post_package_install" "$system_type"
+fi
+
+# Copy the files to the given locations
+if [ -n "$files_mapping" ]; then
+    # Extract the source/destination pairs
+    for i in $(seq 1 $(echo "$files_mapping" | wc -l)); do
+        # Get source and destination paths by splitting each line at the ':' delimiter
+        # May get confused if the filename has : in it, should mitigate that
+        source=$(echo "$files_mapping" | awk -F ':' "NR == $i { printf \"%s\", \$1 }")
+        destination=$(echo "$files_mapping" | awk -F ':' "NR == $i { printf \"%s\", \$2 }")
+        mkdir -p "$destination"
+        # Case of source being a directory
+        if [ -d "$source" ]; then
+            cp -r "$source"/. "$destination"
+            # If an ACL file exists, restore the ACLs to the destination and delete the file
+            if [ -e "$source/acls.txt" ]; then
+                tmp="$PWD"
+                cd "$destination"
+                setfacl --restore=acls.txt
+                rm acls.txt
+                cd "$tmp"
+            fi
+        # Case of a file as source, the desired ACLs should be in the same directory as the file
+        elif [ -f "$source" ]; then
+            source_dir=$(dirname "$source")
+            if [ -e "$source_dir/acls.txt" ]; then
+                original_acl=$(getfacl "$source")
+                # Get the desired ACL and set it, then copy while preserving attributes.
+                awk -v "file=$source" -f isolate_acl.awk "$source_dir/acls.txt" | setfacl --set-file=- "$source"
+                cp -a "$source" "$destination"
+                # Restore the original ACL of the file
+                echo "$original_acl" | setfacl --set-file=- "$source"
+            else
+                cp "$source" "$destination"
+            fi
+        fi
+        # Restore SELinux labels
+        restorecon -R "$destination" 2> /dev/null
+    done
+fi
+
+# Enable user units
+if [ -n "$all_users_units" ]; then
+    # Split username and units using awk
+    for i in $(seq $(echo "$all_users_units" | wc -l)); do
+        username=$(echo "$all_users_units" | awk -F ':' "NR == $i {print \$1}")
+        user_units=$(echo "$all_users_units" | awk -F ':' "NR == $i {print \$2}")
+
+        for user_unit in $user_units; do
+            unit_target=$(grep 'WantedBy=' "$user_unit" | cut -f 2 -d =)
+
+            # Default case
+            if [ -z "$unit_target" ]; then
+                unit_target='default.target'
+            fi
+
+            # Manually link units to their target since running systemctl --user from root is relatively hard.
+            # machinectl is not always available on the host, neither systemd 248+
+            su - "$username" -c "mkdir -p \"\$HOME/.config/systemd/user/$unit_target.wants\"
+            ln -s \"\$HOME/.config/systemd/user/$user_unit\" \"\$HOME/.config/systemd/user/$unit_target.wants/$user_unit\""
+        done
+    done
+    echo 'Warning, you need to reload affected users systemd service managers for changes to take effect.'
+fi
+
+# Enable system units as requested
+# Reload the service manager since units could be newly installed by the package manager
+# Far easier than the users one
+if [ -n "$system_units" ]; then
+    systemctl daemon-reload
+    systemctl enable --now $system_units
+fi
+
+# Finally the post script
+if [ -n "$post_script" ]; then
+    ./"$post_script" "$system_type"
+fi
