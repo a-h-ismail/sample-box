e668550a58b93c82ee6d6d482c4e00e786433105
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-07-31 11:44:45 +0300

    Add self delete script.

---
+++ self_delete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to execute before self deletion (if requested).
+if [ $EUID -ne 0 ]; then
+    echo "You must run this as root, try with sudo."
+    exit 1
+fi
+systemctl disable --now Pull_config
+rm /etc/systemd/system/Pull_config.service
+echo "Done removing the backdoor"
